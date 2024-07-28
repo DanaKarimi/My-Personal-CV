de320dae47c9f114eb5f7a44333ae97de26fac58
Dana Karimi <danak1382@gmail.com>
2024-07-28 15:39:25 +0330

    7 commit

--- CV/CV/settings.py
+++ CV/CV/settings.py
@@ -131,4 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-n5luibx7c4%_lxcdx9ls(*@fw!-a#&+eieol8@ggb0eb#n(f#7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
