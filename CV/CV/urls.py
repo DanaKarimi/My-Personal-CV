2385823b4488d288298c0dbfe7132b03f4b988a9
Dana Karimi <danak1382@gmail.com>
2024-08-03 17:08:26 +0330

    my 7 try commit

--- CV/CV/urls.py
+++ CV/CV/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from homepage.views import HomepageView
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", HomepageView.as_view(),name= 'home'),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
